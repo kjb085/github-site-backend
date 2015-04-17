15985cdf2a721f923913e885c6509ebbc803978b
Kenn Breece <kjb085@gmail.com>
2015-04-17 16:52:27 -0400

    Update the from address

--- application.rb
+++ application.rb
@@ -20,7 +20,7 @@ post '/send_email' do
     p "It's verifying!"
 
     res = Pony.mail(
-      :from => params[:name],
+      :from => params[:name] + "<" + params[:email] + ">",
       :to => 'kjb085@gmail.com',
       :subject => "[kjb085.github.io] " + params[:email],
       :body => params[:message],
